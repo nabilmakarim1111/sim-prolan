f894ed59de18e3bb6ba5d1e4026534ce4fd12b54
nabilmakarim1111 <103266125+nabilmakarim1111@users.noreply.github.com>
2022-04-11 02:09:02 +0700

    Create dosen.cpp

---
+++ source/dosen.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "include/dosen.hpp"
+
+
+dosen::dosen(std::string id, std::string nama, int dd, int mm, int yy, std::string npp, std::string departemen, int pendidikan)
+		: person(id, nama, dd, mm, yy), npp(npp), departemen(departemen), pendidikan(pendidikan)
+{
+}
+
+void dosen::setPendidikan(int pendidikan)
+{
+	this->pendidikan = pendidikan;
+}
+
+int dosen::getPendidikan()
+{
+	return this->pendidikan;
+}
+
+std::string dosen::getDepartemen()
+{
+	return this->departemen;
+}
+
+std::string dosen::getNPP()
+{
+	return this->npp;
+}
